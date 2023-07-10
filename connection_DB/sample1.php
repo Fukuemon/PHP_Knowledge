e9fa819a377c75fe5d08ebe4b964b7c119f1f576
Fukuemon <Fukuemony029@gmail.com>
2023-07-10 14:13:13 +0900

    ⚡️ queryの返り値を利用したsqlでのエラー処理 #1
    - queryメソッドはsql構文の実行に失敗した場合にfalseを返り値として返す
    - sqlでのエラーはphpでは出力されない
    - queryの返り値を変数に入れてif分で処理することで、エラー処理を書くことができる
    ※queryメソッドは変数などと組み合わすのは危険

--- connection_DB/sample1.php
+++ connection_DB/sample1.php
@@ -11,8 +11,14 @@
     $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
     //$db->query("drop table test"); //指定したテーブル(test)を削除する
     $db->query("drop table if exists test"); //指定したテーブル(test)が存在しているなら、削除する
-    $db->query('create table test(id INT)'); //idカラムを持つtestテーブルを作成
-    echo "テーブルを削除して、新規作成しました";
+    $success = $db->query('create table test(id INT)'); //idカラムを持つtestテーブルを作成
+    if ($success) {
+        echo 'テーブルを削除して、新規作成しました';
+    } else {
+        echo "SQLが正常に動作しませんでした";
+        echo $db->error;
+    }
+
     ?>
 </body>
 </html>
