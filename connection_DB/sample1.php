94dfc9883723b582a0a87e4c4bcd2cd68bb9b494
Fukuemon <Fukuemony029@gmail.com>
2023-07-10 14:05:52 +0900

    ⚡️テーブルの削除と作成 #1
    テーブルの削除
    drop table テーブル名
    - if exists をつけることで「存在しているなら削除」ができる

--- connection_DB/sample1.php
+++ connection_DB/sample1.php
@@ -9,10 +9,10 @@
     <?php
     // myspliというモジュールを使用したDB接続
     $db = new mysqli('localhost:8889', 'root', 'root', 'mydb');
-    $db->query('create table test(id INT'); //idカラムを持つtestテーブルを作成
-    echo "テーブルを作成しました";
-
-    echo 'DBに接続しました'
+    //$db->query("drop table test"); //指定したテーブル(test)を削除する
+    $db->query("drop table if exists test"); //指定したテーブル(test)が存在しているなら、削除する
+    $db->query('create table test(id INT)'); //idカラムを持つtestテーブルを作成
+    echo "テーブルを削除して、新規作成しました";
     ?>
 </body>
 </html>
