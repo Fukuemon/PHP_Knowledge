561200e75f5136d6b696a442d387e18b921a04a0
Fukuemon <Fukuemony029@gmail.com>
2023-07-22 14:04:11 +0900

    ✨ phpinfo()：現在のphpの情報を取得　#3

---
+++ PHP_Basic/sample1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+---------------------------
+<?php
+//出力
+echo 'hello world'; //文字列を表示
+echo '<br>'; //改行
+echo 10 + 3; //計算
+echo '<br>';
+//数字：半角と全角
+echo(123); //半角
+echo '<br>';
+echo("123"); //全角(文字列として扱われる)
+?>
+---------------------------
+コメントアウト
+//1行だけコメントアウト
+/**コメントアウト
+ * 複数行
+ * 開業できる
+ */
+---------------------------
+<?php
+phpinfo(); //phpの情報を表示
+?>
+</body>
+</html>
