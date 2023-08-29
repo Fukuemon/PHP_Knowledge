1139f5f312a95a141ebb61aec607339d1050621a
Fukuemon <Fukuemony029@gmail.com>
2023-08-29 23:42:45 +0900

    ✨ validationを行うDB操作 #1
    stmt：sql文
    bind_param：型チェック
    execute：データ追加

--- connection_DB/sample4.php
+++ connection_DB/sample4.php
@@ -1,4 +1,4 @@
-<!--memoの挿入-->
+<!--memoの挿入(validation)-->
 
 <html lang="en">
 <head>
@@ -7,9 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    <?php //データベースに接続
-    $db = new mysqli('localhost:8889', 'root', 'root', 'mydb'); 
-    $ret = $db->query("insert into memos (memo) values ('PHPからのメモです')"); //データを追加
+    <?php 
+    $db = new mysqli('localhost:8889', 'root', 'root', 'mydb'); //データベースに接続
+    $message = "フォームから入力した値"; //フォームから入力した値
+    $stmt = $db->prepare("insert into memos(memo) values(?)"); //データを追加(SQL文)
+    if (!$stmt): //SQL文が間違っていたらエラーを表示
+        die($db->error);
+    endif;
+    $stmt->bind_param('s', $message); //チェックしたい値を設定
+    $ret = $stmt->execute(); //データを追加
     if ($ret):
         echo 'データを追加しました';
     else:
