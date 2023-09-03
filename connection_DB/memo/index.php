05f170f17d25b82808392531325406ee7ba27250
Fukuemon <Fukuemony029@gmail.com>
2023-09-04 07:11:56 +0900

    ✨ ページネーション機能の追加
    limitでよみこむはじめの値と何件読み込むかを指定することができる
    読み込むはじめの値を変化させることで、ページネーション機能を実装する

--- connection_DB/memo/index.php
+++ connection_DB/memo/index.php
@@ -1,10 +1,13 @@
 <?php
 require_once("dbconnect.php");
 
-$memos = $db->query("select * from memos order by id desc"); //order by：並び替え
-if (!$memos) {
-    die($db->error);
-}
+$stmt = $db->prepare("select * from memos order by id desc limit ?, 5"); //order by：並び替え
+if (!$stmt):
+    die($db->error);    
+endif;
+$page = 5;
+$stmt->bind_param("i", $page); 
+$stmt->execute();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,10 +20,11 @@ if (!$memos) {
     <h1>メモ帳</h1>
 
     <p>-> <a href='input.php'>新しいメモ</a></p>
-    <?php while ($memo = $memos->fetch_assoc()): ?>
+    <?php $stmt->bind_result($id, $memo, $created); ?>
+    <?php while ($stmt->fetch()): ?>
     <div>
-        <h2><a href="memo.php?id=<?php echo $memo["id"]; ?>"><?php echo htmlspecialchars(mb_substr($memo["memo"], 0, 50));?></a></h2>
-        <time><?php echo htmlspecialchars($memo["created"]);?></time>
+        <h2><a href="memo.php?id=<?php echo $id; ?>"><?php echo htmlspecialchars(mb_substr($memo, 0, 50));?></a></h2>
+        <time><?php echo htmlspecialchars($created);?></time>
     </div>
     <hr>
     <?php endwhile; ?>
